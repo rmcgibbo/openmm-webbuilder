a1506f5c2263fba395c64a546e510dc8c713f7d6
Robert McGibbon <rmcgibbo@gmail.com>
2013-03-31 01:54:49 -0700

    changed to python from ruby. probably slower, but not having SSL issues and its easier for me

---
+++ app.py
@@ -0,0 +1,93 @@
+import os
+import base64
+import urlparse
+from urllib2 import urlopen
+from urllib import urlencode
+def urldecode(s):
+    return dict(urlparse.parse_qsl(s))
+
+import flask
+from flask import request
+from flask import Response
+from flask import Flask
+
+
+app = Flask(__name__)
+
+##############################################################################
+# Serve index.html
+##############################################################################
+
+@app.route('/')
+def index():
+    return open('public/index.html').read()
+
+
+##############################################################################
+# Serve a redirect page, from a redirect published by github as the 
+# oath redirect
+##############################################################################
+
+@app.route('/login')
+def login():
+    return open('public/login.html').read()
+
+##############################################################################
+# Serve static files
+##############################################################################
+
+@app.route('/js/<filename>')
+def js(filename):
+    return Response(open(os.path.join('public', 'js', filename)).read(),
+                    mimetype='text/javascript')
+
+@app.route('/css/<filename>')
+def css(filename):
+    return Response(open(os.path.join('public', 'css', filename)).read(),
+                    mimetype='text/css')
+
+@app.route('/images/<filename>')
+def images(filename):
+    return open(os.path.join('public', 'images', filename)).read()
+
+@app.route('/templates/<filename>')
+def templates(filename):
+    return open(os.path.join('public', 'templates', filename)).read()
+
+##############################################################################
+# serve /save, which basically ecohos some data as a download
+##############################################################################
+
+@app.route('/save', methods=['POST'])
+def save():
+    val = base64.decodestring(request.form.get('value', ''))
+    filename = request.form.get('filename', 'openmm.py')
+
+    response = Response(val, headers={
+        'Content-Type': 'mime/type',
+        'Content-Description': 'File Transfer',
+        'Content-Disposition': 'attachment; filename=%s' % filename
+    })
+    return response
+
+##############################################################################
+# trade the oauth code for the access token
+##############################################################################
+
+@app.route('/token')
+def github_token():
+    # raise Exception(type())
+    res = urlopen('https://github.com/login/oauth/access_token', data=urlencode({
+            'code': request.args.get('code', ''),
+            'client_id': 'a6a4c15c8e5250bea5c1',
+            'client_secret': '2613f8a86a4aed1c7f87d25eb31a403ad347467f',
+            'grant_type': 'authorization_code'
+    })).read()
+    return urldecode(res)['access_token']
+
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000 (like rake)
+    port = int(os.environ.get('PORT', 5000))
+    app.debug = True
+    app.run(host='0.0.0.0', port=port)
---
+++ public/js/gist.js
@@ -0,0 +1,142 @@
+/*
+ tools to interact with github gist via oAuth.
+ 
+ note: this also depends on the login.html form, which is served
+ by the server on /login, and redirected to via github's oath.
+ 
+ this also depends on the /token endpoint for the server, which
+ can trade a oath "code" for a token
+ */
+
+
+var Gist = function(el) {
+  this._id = undefined;
+  this.base_url = 'https://api.github.com/gists';
+  
+  this.get_token = function () {
+    // get the oauth access token (from localstorage)
+    if (Modernizr.localstorage) {
+      return localStorage.getItem('github-access-token');
+    }
+    return this._token;
+  };
+  
+  this.set_token = function (access_token) {
+    // save the oauth access token (to localstorage)
+    if (Modernizr.localstorage) {
+      localStorage.setItem('github-access-token', access_token);
+    } else {
+      this._token = access_token;
+    }
+  };
+
+  this.get_id = function () {
+    // get the gist's id
+    return this._id;
+  };
+  
+  this.set_id = function (id) {
+    // set the gist's id
+    this._id = id;
+  };
+  
+  this.files = function () {
+    // retreive the content from the dom
+    var rawcode =  $('<div />').html($('#code').data('rawcode')).text();
+    var filename = $('#save-form input[name="filename"]').val();
+    if (filename == '') {
+      filename = $('#save-form input[name="filename"]').attr('placeholder');
+    }
+
+    values = {}
+    values[filename] = {'content': rawcode}
+    return values;
+  };
+  
+  this.post_gist = function () {
+    // post the gist to github
+    self = this;
+
+    $.post(this.base_url + '?access_token=' + this.get_token(),
+      JSON.stringify({public: true, files: this.files()}),
+      function (gist) {
+        
+        $('#navbar-gist').prepend('<a id="view-gist" href="' + gist.html_url + '" target="_blank" class="btn btn-success">View</a>');
+        $('#save-script-gist').html('Update Gist');
+        self.set_id(gist.id);
+        // console.log(gist.html_url);
+        // console.log('post succeeded');
+      }
+    );
+  };
+  
+  this.update_gist = function () {
+    // update the gist on github
+    var url = this.base_url + '/' + this.get_id() + '?access_token=' + this.get_token();
+    
+    self = this;
+    $.ajax(url, {
+      type: 'PATCH',
+      data: JSON.stringify({files: this.files()}),
+      success: function (gist) {
+        bootbox.alert('Update successful!');
+      },
+      error: function (jqXHR, error, message) {
+        // if the update fails, we display an error message, remove the 'view'
+        // button, and change the 'Update Gist' button to 'Save Gist'
+        $('#save-script-gist').html('Save Gist');
+        $('#view-gist').remove()
+        // this is needed so that future touches to the Save Gist button
+        // tries to run the post_gist method, not the update_gist method
+        self.set_id(undefined);
+        bootbox.dialog('There was an error updating your gist.<br/><br/>Error: ' + message,
+          [{
+          'class': 'btn-danger',
+          'label': 'OK'
+          }]);
+      }
+    });
+    
+  };
+} // end class
+
+
+// main method that uses the class
+$(function () {
+  
+  // place a new 
+  $('.navbar ul.nav').after(' \
+  <ul id="navbar-gist" class="navbar-form pull-right" style="padding-right:120px" > \
+    <button id="save-script-gist" type="submit" class="btn">Save Gist</button> \
+  </ul>');
+
+  var gist = new Gist();
+  // Step 4
+  window.addEventListener('message', function (event) {
+    var code = event.data;
+    // Step 5
+    $.get('token?code=' + code, function (access_token) {
+      // Step 7
+      gist.set_token(access_token);
+      gist.post_gist();
+    });
+  });
+
+  $('#save-script-gist').click(function () {
+    if (gist.get_token() == undefined) {
+      //Step 2
+      window.open('https://github.com' + 
+        '/login/oauth/authorize' + 
+        '?client_id=a6a4c15c8e5250bea5c1' +
+        '&scope=gist');
+        return;
+    }
+
+    if (gist.get_id()) {
+      gist.update_gist();
+    } else {
+      gist.post_gist();
+    }
+    
+  });
+});
